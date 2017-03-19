fd1334c73ba6e103f6d074b6f3badcf06c7821d9
WAR <alex.kopylov.08@gmail.com>
2017-03-19 22:58:44 +0200

    change index.html, mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $method = $_SERVER['REQUEST_METHOD'];
 //Script Foreach
 $c = true;
-if ( $method === 'POST' ) {
+if ( $method === 'POST' ) {	
 	$project_name = trim($_POST["project_name"]);
 	$admin_email  = trim($_POST["admin_email"]);
 	$form_subject = trim($_POST["form_subject"]);
